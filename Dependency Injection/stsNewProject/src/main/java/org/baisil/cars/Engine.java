f02160a452895853403637117a81eceb386748cd
baisilg <pgbaisil@gmail.com>
2020-06-13 09:14:47 +0530

    created  the getters and setters

--- Dependency Injection/stsNewProject/src/main/java/org/baisil/cars/Engine.java
+++ Dependency Injection/stsNewProject/src/main/java/org/baisil/cars/Engine.java
@@ -1,5 +1,26 @@
 package org.baisil.cars;
 
 public class Engine {
+	
+	
+	String type;// this class will have the property in the name of the "type"....
+	
+	public Engine() {
+		
+		type = "V8";
+		
+		
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	 
 
 }
